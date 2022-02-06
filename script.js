d34cb873d260bb82de7e4cd3c7adfc63c37e6c50
anjali-th <anjalithing@gmail.com>
2022-02-06 10:50:31 -0600

    Checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// Your JS goes here
+for ( let i = 0; i < 63; i++){
+    const div = document.createElement('div');
+    div.style.width = '11.1%';
+    div.style.paddingBottom = '11.1%';
+    div.style.float = 'left';
+    div.style.backgroundColor = i % 2 === 0 ? 'black' : 'red';
+    document.body.append(div);
+}
+   
